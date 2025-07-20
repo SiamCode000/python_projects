a5833bfe91b9c68fdc09730dc4fccfcf5bf4c7b0
SiamCode000 <siampro2021@gmail.com>
2025-07-20 15:10:33 +0600

    20/7/25

---
+++ WEATHER-APP/main.py
@@ -0,0 +1,14 @@
+import requests
+import json
+while True:
+    try:
+        city = input("Enter the city name: ")
+
+        url = f"http://api.weatherapi.com/v1/current.json?key=0795de5375ac40f09b475657252007&q={city}&aqi=no"
+        r = requests.get(url)
+        weatherDict = json.loads(r.text)
+        parse = weatherDict["current"]["temp_c"]
+        print(parse)
+    except Exception as e:
+        print("No matching Location found")
+        quit()
